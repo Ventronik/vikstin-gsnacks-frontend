655cbb48ca23262c4e84773c7f67701026c52356
Dustin Ruskell <druskell@gmail.com>
2018-05-31 09:13:58 -0700

    most of a delete route built

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -1,6 +1,7 @@
 import request from '../helpers/request';
 
 export const GET_REVIEWS = 'GET_REVIEWS';
+export const DELETE_REVIEW = 'DELETE_REVIEW';
 
 export const getReviews = () => {
   return dispatch => {
@@ -8,6 +9,19 @@ export const getReviews = () => {
     .then(response => {
       dispatch({
         type: GET_REVIEWS,
+        payload: response.data.data
+      });
+    });
+  }
+};
+
+export const deleteReview = (id) => {
+  console.log('reviews in actions id: ', id)
+  return dispatch => {
+    request(`/api/reviews/${id}`, 'delete')
+    .then(response => {
+      dispatch({
+        type: DELETE_REVIEW,
         payload: response.data.data
       });
     });
