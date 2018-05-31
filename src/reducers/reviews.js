c4b47bd8db97fd82c03030632435e40e5ef208a5
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-05-31 12:33:10 -0700

    updated review delete so it gets all reviews too

--- src/reducers/reviews.js
+++ src/reducers/reviews.js
@@ -12,7 +12,7 @@ export default (state = initialState, action) => {
     case GET_REVIEWS:
       return {...state, reviews: action.payload};
     case DELETE_REVIEW:
-      return {...state};
+      return {...state, reviews: action.payload};
     default:
       return state;
   }
